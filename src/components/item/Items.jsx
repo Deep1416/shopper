f763af42e105faf20210034cbd705031def70c9a
deep1416 <deependra186037@gmail.com>
2025-01-03 12:35:48 +0530

    done

---
+++ src/components/item/Items.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Items = (props) => {
+  return (
+    <div className="w-[260px] h-[360px]">
+      <Link to={`/product/${props.id}`}>
+        {" "}
+        <div className="overflow-hidden w-full h-[240px]">
+          <img
+            onClick={window.scrollTo(0, 0)}
+            className="item"
+            src={props.image}
+            alt=""
+          />
+        </div>
+      </Link>
+      <p className="my-1">{props.name}</p>
+      <div className="flex gap-5">
+        <div className="text-[#374151] text-lg font-semibold">
+          ${props.new_price}
+        </div>
+        <div className="text-[#8c8c8c] text-lg font-medium line-through ">
+          ${props.old_price}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Items;
